ff1aa6cb33b25ca029cdbf2fefe13bb48aba7d64
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-10-22 07:17:27 -0700

    [check.js] Write function 5 times

--- check.js
+++ check.js
@@ -1,6 +1,6 @@
 function checkNumber(num) {
     if (num > 0) {
-        return 'positive'
+        return 'positive';
     } else if (num < 0) {
         return 'negative';
     } else {
@@ -9,3 +9,54 @@ function checkNumber(num) {
 }
 
 console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(10));
+
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(-10));
+
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(22));
+
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+
+console.log(checkNumber(0));
