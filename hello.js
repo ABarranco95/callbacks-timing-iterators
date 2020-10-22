24f0b16eb92c10dd705a059b8c458eb7196db57d
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-10-22 06:59:32 -0700

    [hello.js] Add function 5 times.

---
+++ hello.js
@@ -0,0 +1,40 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Angel'));
+
+
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('Rome'));
+
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?'
+    return msg;
+}
+console.log(sayHello('Pete'));
+
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('Dexter'));
+
+
+
